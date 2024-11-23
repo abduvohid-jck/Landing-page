e712e394e8852d159e69485d288f5406e6c2036e
abduvohid-jck <abduvohidmurodov26@gmail.com>
2024-11-23 17:47:26 +0500

    Landing page project completed

---
+++ js/index.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+    theme: {
+        container: {
+          center: true,
+          width:{
+            tablet:"682px",
+            desktop:"1170px",
+          },
+          padding:{
+            DEFAULT: "30px",
+            tablet: "0",
+          },
+        },
+        fontFamily: {
+          poppins:["poppins"],
+          mulish:["mulish"],
+        },
+        screens:{
+            tablet: "780px",
+            desktop: "1280px",
+        },
+    },
+  };
+  
